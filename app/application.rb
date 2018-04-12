7eef2ae317335d96ba5cb3cf2e684b351b007cbb
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-12 06:08:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,32 +1,30 @@
 class Application
-
-  @@items = ["Apples","Carrots","Pears"]
+ 	 
+   @@items = ["Apples","Carrots","Pears"]	   @@items = ["Apples","Carrots","Pears"]
   @@cart = []
-
-  def call(env)
-    resp = Rack::Response.new
-
-
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-
+ 	 
+   def call(env)	   def call(env)
+     resp = Rack::Response.new	     resp = Rack::Response.new
+@@ -13,6 +14,22 @@ def call(env)
+     elsif req.path.match(/search/)	     elsif req.path.match(/search/)
+       search_term = req.params["q"]	       search_term = req.params["q"]
+       resp.write handle_search(search_term)	       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-     If @@cart.empty?
-     resp.write "Your cart is empty"
-    else
-      @@cart.each do |item|
-        resp.write "#{item}\n"
-    end
-end
-elsif req.path.match(/add/)
-  item_to_add = req.params["item"]
-  if @@items.include? item_to_add
-    @@cart << item_to_add
-    resp.write "added #{item_to_add}"
-  else
-    resp.write "We don't have that item!"
-  end
-else
-    resp.write "Path not Found"
-  end
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      if @@items.include? item_to_add 
+        @@cart << item_to_add
+        resp.write "added #{item_to_add}"
+      else
+        resp.write "We don't have that item!"
+      end
+     else	     else
+       resp.write "Path Not Found"	       resp.write "Path Not Found"
+     end	     end
